8aba70c830a3e65d6d8da993d074b38934a02aeb
Hicanos <ruda32168@gmail.com>
2025-04-21 19:58:41 +0900

    feat: 이름입력, 직업선택
    캐릭터 생성 메서드 추가
    스탯, 정보는 나중에 설정에 따라 바꿀 것

--- TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fe7b8f744fcada11b42989fa3900d7d6486b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40cc570196e609245cf303572a13e8391c388f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
