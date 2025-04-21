4584723bd9ab862ec176ada35ee999e73ccdc40f
Duek1 <ppsik22@gmail.com>
2025-04-21 20:12:19 +0900

    aaa
    aaa

--- TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40cc570196e609245cf303572a13e8391c388f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33c487fa6aeca4df04edbe54e2e47c1e208d37b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
