87d919e80e5ecdb95438e15196dc2654cff792b1
Duek1 <ppsik22@gmail.com>
2025-04-21 19:09:43 +0900

    solution
    solution

--- TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c85b36aad2ee129aa81ffb17c63f4c2f8d66767e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17f91a21e1c793562e5a58bd338fedc595f04593")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
