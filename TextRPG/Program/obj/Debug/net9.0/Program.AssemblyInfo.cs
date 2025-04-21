b8d99f9eafd62fde75514564bb3b951f7625227a
Hicanos <ruda32168@gmail.com>
2025-04-21 16:50:39 +0900

    ?: 이거뭐지
    커밋하니까 받았습니다.

--- TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4710b131fb014db9c4a91d10de55c16555d1de3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c85b36aad2ee129aa81ffb17c63f4c2f8d66767e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
