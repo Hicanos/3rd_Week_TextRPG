a92227adf4300b2428e3d990ba56f4e607a39424
jellujoa <jellujoa03@gmail.com>
2025-04-22 18:01:16 +0900

    Update Quest.cs

--- TextRPG/Program/Quest.cs
+++ TextRPG/Program/Quest.cs
@@ -11,7 +11,29 @@ namespace Quest
 {
    public class Quest
     {
+        public string questName; // 퀘스트 이름
+        public string questStroy: // 퀘스트 스토리 
+         // public int (보상)    
+        public string questPerfoem; // 수행내역
+        public qusetstatus ; // 진행 스텟  
+        
 
+       public void questName()
+        {
+            Console.WriteLine("퀘스트 분류\n");
+            Console.WriteLine("1.신입 퀘스트");
+            Console.WriteLine("2.경력직 퀘스트");
+            Console.WriteLine("3.사장 퀘스트");
+            Console.WriteLine("4.칭호 퀘스트");
+            Console.WriteLine("5.메뉴로 돌아가기");
+            Console.ReadLine();
+
+
+        }
+    }
+    public class RunQ 
+    {
+        quwstName();
     }
 }
             
