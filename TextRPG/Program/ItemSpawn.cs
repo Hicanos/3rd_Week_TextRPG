d00e066ccdbfd18a16ac6770c272258a5e8be4cd
JaeHyuck-Suc <socosj7801@gmail.com>
2025-04-26 15:14:28 +0900

    Feat : 소모품 아이템 리스트 추가
    일회제한 아이템 및 무제한 아이템 추가

--- TextRPG/Program/ItemSpawn.cs
+++ TextRPG/Program/ItemSpawn.cs
@@ -17,47 +17,60 @@ namespace TextRPG.ItemSpawnManagement
 
             // 무기
             new Weapons(false, false, "최신 스마트폰", new Dictionary<string, int> { { "공격력", 12 }, { "명중율", 15 }, { "치명타 확률", 8 } }, "최신형 스마트폰. 강력한 성능!", "전체", "무기", 2500);
-            new Weapons(false, false, "맥 북", new Dictionary<string, int> { { "공격력", 15 }, { "마나", 30 }, { "치명타 배율", 25 } }, "귀엽고 아담한 중고 맥 북", "전체", "무기", 3400);
+            new Weapons(false, false, "맥 북", new Dictionary<string, int> { { "공격력", 15 }, { "MaxMP", 30 }, { "치명타 배율", 25 } }, "귀엽고 아담한 중고 맥 북", "전체", "무기", 3400);
             new Weapons(false, false, "무선 마우스 키보드", new Dictionary<string, int> { { "공격력", 18 }, { "치명타 배율", 20 }, { "치명타 확률", 10 } }, "무선이라 자유롭다.", "전체", "무기", 4200);
-            new Weapons(false, false, "그래픽카드5090", new Dictionary<string, int> { { "공격력", 20 }, { "치명타 배율", 50 }, { "마나", 90 } }, "어 형은 5090오우너야~", "전체", "무기", 5090);
+            new Weapons(false, false, "그래픽카드5090", new Dictionary<string, int> { { "공격력", 20 }, { "치명타 배율", 50 }, { "MaxMP", 90 } }, "어 형은 5090오우너야~", "전체", "무기", 5090);
             new Weapons(false, false, "Moo나미볼펜", new Dictionary<string, int> { { "공격력", 2 }, { "치명타 확률", 3 } }, "사무실에 하나씩은 있는 그 볼펜", "전체", "무기", 500);
             new Weapons(false, false, "형광펜", new Dictionary<string, int> { { "공격력", 5 }, { "치명타 배율", 15 } }, "슥슥 잘 그려지는 색상 형광펜", "전체", "무기", 1100);
-            new Weapons(false, false, "빗자루&쓰레받이", new Dictionary<string, int> { { "공격력", 7 }, { "마나", 20 }, { "치명타 배율", 20 } }, "왜? 너도 청소해주랴?", "전체", "무기", 1800);
+            new Weapons(false, false, "빗자루&쓰레받이", new Dictionary<string, int> { { "공격력", 7 }, { "MaxMP", 20 }, { "치명타 배율", 20 } }, "왜? 너도 청소해주랴?", "전체", "무기", 1800);
             new Weapons(false, false, "결제 화일", new Dictionary<string, int> { { "공격력", 17 }, { "방어력", -2 }, { "치명타 확률", -5 } }, "인사팀이 결제받을 때 쓰는 잇템.", "인사팀", "무기", 3000);
-            new Weapons(false, false, "명함", new Dictionary<string, int> { { "공격력", 12 }, { "마나", 40 }, { "치명타 확률", -8 } }, "홍보팀 표준 명함. 소재가 튼튼해서 날리면 은근 아프다.", "홍보팀", "무기", 3000);
-            new Weapons(false, false, "계산기", new Dictionary<string, int> { { "공격력", 16 }, { "마나", -15 }, { "회피율", -10 } }, "인간 계산기 경지에 오르면 무기로 밖에 쓸모없는 아이템.", "총무팀", "무기", 3000);
+            new Weapons(false, false, "명함", new Dictionary<string, int> { { "공격력", 12 }, { "MaxMP", 40 }, { "치명타 확률", -8 } }, "홍보팀 표준 명함. 소재가 튼튼해서 날리면 은근 아프다.", "홍보팀", "무기", 3000);
+            new Weapons(false, false, "계산기", new Dictionary<string, int> { { "공격력", 16 }, { "MaxMP", -15 }, { "회피율", -10 } }, "인간 계산기 경지에 오르면 무기로 밖에 쓸모없는 아이템.", "총무팀", "무기", 3000);
             new Weapons(false, false, "커터칼", new Dictionary<string, int> { { "공격력", 14 }, { "명중률", -15 }, { "치명타 배율", 10 } }, "주로 박스를 자를 때 쓴다. 가끔은 누구의 명줄을 자를때도....", "영업팀", "무기", 3000);
-            new Weapons(false, false, "만년필", new Dictionary<string, int> { { "공격력", 18 }, { "마나", -20 }, { "명중률", -5 } }, "누가 쓰는지는 모르겠지만 있어보이는 아이템.", "전산팀", "무기", 3000);
+            new Weapons(false, false, "만년필", new Dictionary<string, int> { { "공격력", 18 }, { "MaxMP", -20 }, { "명중률", -5 } }, "누가 쓰는지는 모르겠지만 있어보이는 아이템.", "전산팀", "무기", 3000);
             new Weapons(false, false, "클립 집게", new Dictionary<string, int> { { "공격력", 14 }, { "명중률", -10 }, { "치명타 확률", 10 } }, "넌 이미 찝혀있다!", "기획팀", "무기", 3000);
 
             // 방어구
             new Weapons(false, false, "싸구려 정장 상의", new Dictionary<string, int> { { "방어력", 3 }, { "회피율", 4 } }, "저렴한 정장 상의.", "전체", "상의", 900);
-            new Weapons(false, false, "통풍셔츠", new Dictionary<string, int> { { "방어력", 6 }, { "체력", 10 }, { "마나", 25 }, { "치명타 확률", 10 } }, "시원하고 방어도 가능한 셔츠.", "전체", "상의", 2400);
-            new Weapons(false, false, "명품 정장 상의", new Dictionary<string, int> { { "공격력", 4 }, { "방어력", 10 }, { "치명타 확률", 10 }, { "체력", 25 } }, "명품 브랜드의 최고 인기 상품.", "전체", "상의", 8000);
-            new Weapons(false, false, "싸구려 정장 하의", new Dictionary<string, int> { { "방어력", 3 }, { "마나", 15 } }, "저렴한 정장 바지.", "전체", "하의", 800);
-            new Weapons(false, false, "반바지", new Dictionary<string, int> { { "방어력", 5 }, { "체력", 10 }, { "명중률", 10 }, { "회피율", 10 } }, "여름에 좋은 반바지.", "전체", "하의", 2200);
-            new Weapons(false, false, "명품 정장 하의", new Dictionary<string, int> { { "공격력", 5 }, { "방어력", 7 }, { "치명타 배율", 30 }, { "체력", 20 } }, "명품 브랜드의 역대급 상품 .", "전체", "하의", 8000);
+            new Weapons(false, false, "통풍셔츠", new Dictionary<string, int> { { "방어력", 6 }, { "MaxHP", 10 }, { "MaxMP", 25 }, { "치명타 확률", 10 } }, "시원하고 방어도 가능한 셔츠.", "전체", "상의", 2400);
+            new Weapons(false, false, "명품 정장 상의", new Dictionary<string, int> { { "공격력", 4 }, { "방어력", 10 }, { "치명타 확률", 10 }, { "MaxHP", 25 } }, "명품 브랜드의 최고 인기 상품.", "전체", "상의", 8000);
+            new Weapons(false, false, "싸구려 정장 하의", new Dictionary<string, int> { { "방어력", 3 }, { "MaxMP", 15 } }, "저렴한 정장 바지.", "전체", "하의", 800);
+            new Weapons(false, false, "반바지", new Dictionary<string, int> { { "방어력", 5 }, { "MaxHP", 10 }, { "명중률", 10 }, { "회피율", 10 } }, "여름에 좋은 반바지.", "전체", "하의", 2200);
+            new Weapons(false, false, "명품 정장 하의", new Dictionary<string, int> { { "공격력", 5 }, { "방어력", 7 }, { "치명타 배율", 30 }, { "MaxHP", 20 } }, "명품 브랜드의 역대급 상품 .", "전체", "하의", 8000);
             new Weapons(false, false, "발가락 양말", new Dictionary<string, int> { { "방어력", 2 }, { "회피율", 5 } }, "편안한 양말.", "전체", "신발", 1000);
-            new Weapons(false, false, "크록스", new Dictionary<string, int> { { "방어력", 3 }, { "체력", 10 }, { "치명타 배율", 20 }, { "마나", 15 } }, "편안한 크록스.", "전체", "신발", 2000);
+            new Weapons(false, false, "크록스", new Dictionary<string, int> { { "방어력", 3 }, { "MaxHP", 10 }, { "치명타 배율", 20 }, { "MaxMP", 15 } }, "편안한 크록스.", "전체", "신발", 2000);
             new Weapons(false, false, "냄새나는 구두", new Dictionary<string, int> { { "공격력", 4 }, { "명중률", -10 }, { "치명타 배율", 25 } }, "혹시 자신의 발냄새를 맡는 사람이 있다면 조심해라! 습...하아~", "전체", "신발", 1000);
             new Weapons(false, false, "명품 구두", new Dictionary<string, int> { { "공격력", 5 }, { "방어력", 4 }, { "명중률", 15 }, { "회피율", 15 } }, "명품 브랜드의 티가 날듯 안날듯한 구두.", "전체", "신발", 8000);
 
             // 악세서리
-            new Weapons(false, false, "알없는 안경", new Dictionary<string, int> { { "방어력", 5 }, { "마나", 20 }, { "명중률", -10 } }, "기본적인 안경.", "총무팀", "장신구", 2300);
+            new Weapons(false, false, "알없는 안경", new Dictionary<string, int> { { "방어력", 5 }, { "MaxMP", 20 }, { "명중률", -10 } }, "기본적인 안경.", "총무팀", "장신구", 2300);
             new Weapons(false, false, "서류철", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 2 }, { "명중률", 5 } }, "인사팀의 중요한 서류.", "인사팀", "장신구", 1500);
             new Weapons(false, false, "테이프", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 2 }, { "회피율", 3 } }, "홍보팀의 필수 아이템.", "홍보팀", "장신구", 1500);
             new Weapons(false, false, "C타입 충전기", new Dictionary<string, int> { { "공격력", 3 }, { "방어력", 3 }, { "치명타 확률", -5 } }, "총무팀의 중요한 장비.", "총무팀", "장신구", 1500);
             new Weapons(false, false, "USB", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 3 } }, "영업팀의 필수 장비.", "영업팀", "장신구", 1500);
-            new Weapons(false, false, "보조배터리", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 2 }, { "마나", 15 } }, "전산팀의 중요한 장비.", "전산팀", "장신구", 1500);
+            new Weapons(false, false, "보조배터리", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 2 }, { "MaxMP", 15 } }, "전산팀의 중요한 장비.", "전산팀", "장신구", 1500);
             new Weapons(false, false, "건전지", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 2 }, { "치명타 확률", 3 } }, "기획팀의 필수 아이템.", "기획팀", "장신구", 1500);
-            new Weapons(false, false, "명품 시계", new Dictionary<string, int> { { "공격력", 5 }, { "방어력", 5 }, { "마나", 50 }, { "치명타 배율", 30 } }, "명품 브랜드의 희귀 컬렉션 시계.", "전체", "장신구", 8000);
-            new Weapons(false, false, "A4용지", new Dictionary<string, int> { { "마나", 15 }, { "치명타 확률", 5 } }, "기본적인 A4용지.", "전체", "장신구", 900);
-            new Weapons(false, false, "에어팟", new Dictionary<string, int> { { "마나", 50 }, { "방어력", -3 }, { "회피율", 25 } }, "MZ 사원들의 필수템! 네? 부장님 머라고요?", "전체", "장신구", 1500);
+            new Weapons(false, false, "명품 시계", new Dictionary<string, int> { { "공격력", 5 }, { "방어력", 5 }, { "MaxMP", 50 }, { "치명타 배율", 30 } }, "명품 브랜드의 희귀 컬렉션 시계.", "전체", "장신구", 8000);
+            new Weapons(false, false, "A4용지", new Dictionary<string, int> { { "MaxMP", 15 }, { "치명타 확률", 5 } }, "기본적인 A4용지.", "전체", "장신구", 900);
+            new Weapons(false, false, "에어팟", new Dictionary<string, int> { { "MaxMP", 50 }, { "방어력", -3 }, { "회피율", 25 } }, "MZ 사원들의 필수템! 네? 부장님 머라고요?", "전체", "장신구", 1500);
             new Weapons(false, false, "노트북백팩", new Dictionary<string, int> { { "공격력", 4 }, { "방어력", 4 }, { "치명타 확률", 15 } }, "노트북과 같이 구매하기 좋은 세트 상품", "전체", "장신구", 3300);
 
-            // 물약 (임시)
-            new Weapons(false, false, "반창고", new Dictionary<string, int> { { "HP", 50 } }, "작은 상처는 반창고 하나면 충분!", "전체", "포션", 300);
-            new Weapons(false, false, "바까스", new Dictionary<string, int> { { "HP", 30 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 300, true);
+            // 소모품
+            new Weapons(false, false, "반창고", new Dictionary<string, int> { { "HP", 50 } }, "작은 상처는 반창고 하나면 충분!", "전체", "포션", 600);
+            new Weapons(false, false, "멸균붕대", new Dictionary<string, int> { { "HP", 50 } }, "사무실마다 하나씩은 있는 그 붕대", "전체", "포션", 1000);
+            new Weapons(false, false, "구급 상자", new Dictionary<string, int> { { "HP", 50 } }, "작은 상처는 반창고 하나면 충분!", "전체", "포션", 1500);
+            new Weapons(false, false, "바까스", new Dictionary<string, int> { { "MP", 30 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 600);
+            new Weapons(false, false, "아메리카노", new Dictionary<string, int> { { "MP", 100 } }, "직장인들의 힐링포션", "전체", "포션", 1500);
+            new Weapons(false, false, "장어구이", new Dictionary<string, int> { { "공격력", 3 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+            new Weapons(false, false, "흑마늘", new Dictionary<string, int> { { "방어력", 3 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+            new Weapons(false, false, "삼계탕", new Dictionary<string, int> { { "MaxHP", 20 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+            new Weapons(false, false, "복분자주", new Dictionary<string, int> { { "MaxMP", 15 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+            new Weapons(false, false, "블루베리 스무디", new Dictionary<string, int> { { "명중률", 10 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+            new Weapons(false, false, "연어회", new Dictionary<string, int> { { "회피율", 8 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+            new Weapons(false, false, "다크 초콜렛", new Dictionary<string, int> { { "치명타 배율", 10 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+            new Weapons(false, false, "홍삼젤리", new Dictionary<string, int> { { "치명타 확률", 5 } }, "피로회복제 그 이름, 바까스!", "전체", "포션", 1000, true);
+
+
         }
 
         public static void NotBuyAbleWeaponSpawn()
@@ -66,9 +79,9 @@ namespace TextRPG.ItemSpawnManagement
 
             new Weapons(false, false, "텀블러", new Dictionary<string, int> { { "공격력", 10 }, { "치명타 확률", 10 } }, "개인 텀블러는 직장인 필수템!", "전체", "무기", "사장", 15, 1500);
             new Weapons(false, false, "브랜드 정장 상의", new Dictionary<string, int> { { "방어력", 5 }, { "명중률", 10 }, { "치명타", 20 } }, "고급스러움이 풍기는 상의.", "전체", "상의", "전무", 16, 1500);
-            new Weapons(false, false, "브랜드 정장 하의", new Dictionary<string, int> { { "방어력", 5 }, { "체력", 15}, { "치명타 확률", 10 } }, "편안함과 스타일을 동시에.", "전체", "하의", "상무", 17, 1500);
-            new Weapons(false, false, "브랜드 구두", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 2 }, { "마나", 15 } }, "고급 가죽으로 제작된 구두.", "전체", "신발", "이사", 18, 1500);
-            new Weapons(false, false, "방석&등받이 쿠션", new Dictionary<string, int> { { "방어력", 2 }, { "체력", 5 }, { "회피율", 10 } }, "오래 앉아 있어도 괜찮다!", "전체", "장신구", "실장", 19, 1500);
+            new Weapons(false, false, "브랜드 정장 하의", new Dictionary<string, int> { { "방어력", 5 }, { "MaxHP", 15}, { "치명타 확률", 10 } }, "편안함과 스타일을 동시에.", "전체", "하의", "상무", 17, 1500);
+            new Weapons(false, false, "브랜드 구두", new Dictionary<string, int> { { "공격력", 2 }, { "방어력", 2 }, { "MaxMP", 15 } }, "고급 가죽으로 제작된 구두.", "전체", "신발", "이사", 18, 1500);
+            new Weapons(false, false, "방석&등받이 쿠션", new Dictionary<string, int> { { "방어력", 2 }, { "MaxHP", 5 }, { "회피율", 10 } }, "오래 앉아 있어도 괜찮다!", "전체", "장신구", "실장", 19, 1500);
             new Weapons(false, false, "든든한 국밥", new Dictionary<string, int> { { "MaxHP", 10 }, { "MaxMP", 10 } }, "마음까지 든든해진다.", "전체", "포션", "전무, 사장, 회장", 5, 1000);
         }
 
